272e7e5addb49b008e787e176e4e8498f1f1a96f
Nick <scopezsxex@gmail.com>
2024-11-04 17:40:26 -0500

    carmodels

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
     # path for dealer reviews view
     path(route="logout/", view=views.logout_request, name="logout"),
     # path for add a review view
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
